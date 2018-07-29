66071168fc4b453a7a42e609231c56d14b4276d3
darth-c0d3r <gurparkashsohi@gmail.com>
2018-07-29 21:45:01 +0530

    duh

--- assignment1/Q2/codes/myAHE.m
+++ assignment1/Q2/codes/myAHE.m
@@ -1,22 +1,34 @@
-function myAHE(img_name, n)
-  img = imread(img_name);
+function myAHE(img, n)
+%  img = imread(img_name);
   [w,h,d] = size(img);
+
   img_out = img;
-%  i=1:w;
-%  j=1:h;
-  
+  sz = ceil((n-1)/2);
+  flag = 0;
+  rng_j = 1:(h-n);
   for k=1:d
-    for i=500:700
-      ymin=max(1,i-ceil((n-1)/2));
-      ymax=min(i+ceil((n-1)/2), w);
-      for j=500:700
-        xmin=max(1,j-ceil((n-1)/2));
-        xmax=min(j+ceil((n-1)/2), h);
-        part = img(ymin:ymax, xmin:xmax,k);
-        img_out(i,j,k) = myAHE_helper(part,i-ymin+1,j-xmin+1);
+    histgm = myAHE_helper(img(1:n,1:n,k));
+    for i=1:(w+1-n)
+      rng_i = i:(i+n-1);
+      cdf = cumsum(histgm);
+      cdf = (cdf/cdf(256))*255;
+      img_out(i+sz,rng_j(1)+sz,k) = cdf(img(i+sz,rng_j(1)+sz,k)+1);
+      for j=rng_j
+        if flag == 0
+          histgm -= myAHE_helper(img(rng_i,j,k));
+          histgm += myAHE_helper(img(rng_i,j+n,k));
+        else
+          histgm += myAHE_helper(img(rng_i,j,k));
+          histgm -= myAHE_helper(img(rng_i,j+n,k));
+        end
+        cdf = cumsum(histgm);
+        cdf = (cdf/cdf(256))*255;
+        img_out(i+sz,j+1+sz,k) = cdf(img(i+sz,j+1+sz,k)+1);
       end
+      rng_j = fliplr(rng_j);
+      flag = 1-flag;
     end
   end
   visualize(img_out);
   
-endfunction
+end
--- assignment1/Q2/codes/myAHE_helper.m
+++ assignment1/Q2/codes/myAHE_helper.m
@@ -1,13 +1,7 @@
-function pix_out = myAHE_helper(img,p,q)
-  [w,h,d] = size(img);
-  img_out = img;
+function histgm = myAHE_helper(img)
+  [w,h] = size(img);
   i=1:w;
   j=1:h;
-  hist_ = zeros(1,256);
   x = reshape(img(i,j), 1, w*h);
-  [a,b] = hist(x, unique(x)); 
-  hist_(b+1) = a;
-  cdf = cumsum(hist_);
-  cdf = (cdf/cdf(256))*255;
-  pix_out = cdf(img(p,q)+1);
-endfunction
+  histgm = histc(x, 0:255);
+end
