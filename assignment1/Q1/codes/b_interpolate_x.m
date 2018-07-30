728b343998354be403759d1100938d1cdf6244af
darth-c0d3r <gurparkashsohi@gmail.com>
2018-07-31 03:40:41 +0530

    make code matlab compatible

--- assignment1/Q1/codes/b_interpolate_x.m
+++ assignment1/Q1/codes/b_interpolate_x.m
@@ -2,8 +2,12 @@ function img = b_interpolate_x(image, factor, yjump)
   [M,N] = size(image);
   img = image;
   i_rng = 1:yjump:M;
-  for right=factor+1:factor:N
+  for right=(factor+1):factor:N
     left=right-factor;
-    img(i_rng,left:right) = linspace(image(i_rng,left), image(i_rng,right), factor+1);
+    m = (image(i_rng,right) - image(i_rng,left))/(factor);
+%     img(i_rng,left:right) = linspace(image(i_rng,left), image(i_rng,right), factor+1);
+%     disp(image(i_rng,left):m:image(i_rng,right));
+%     disp(img(i_rng,left:right));
+    img(i_rng,left:right) = image(i_rng,left):m:image(i_rng,right);
   end
 end
