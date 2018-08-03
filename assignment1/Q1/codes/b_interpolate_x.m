c7676068a65d83f115c7717cb551b6b07d9762c6
darth-c0d3r <gurparkashsohi.06@gmail.com>
2018-08-03 19:12:20 +0530

    b_interpolate linspace corrected

--- assignment1/Q1/codes/b_interpolate_x.m
+++ assignment1/Q1/codes/b_interpolate_x.m
@@ -3,11 +3,9 @@ function img = b_interpolate_x(image, factor, yjump)
   img = image;
   i_rng = 1:yjump:M;
   for right=(factor+1):factor:N
-    left=right-factor;
-    m = (image(i_rng,right) - image(i_rng,left))/(factor);
-%     img(i_rng,left:right) = linspace(image(i_rng,left), image(i_rng,right), factor+1);
-%     disp(image(i_rng,left):m:image(i_rng,right));
-%     disp(img(i_rng,left:right));
-    img(i_rng,left:right) = image(i_rng,left):m:image(i_rng,right);
+  	for i=i_rng
+	    left=right-factor;
+	    img(i,left:right) = linspace(image(i,left), image(i,right), factor+1);
+    end
   end
 end
