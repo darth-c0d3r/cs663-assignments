e815f56e9a7207afd40cfc9de112f9da4cda68a0
darth-c0d3r <gurparkashsohi@gmail.com>
2018-08-03 23:53:08 +0530

    q2c (myHM.m) done

--- assignment1/Q2/codes/myHM.m
+++ assignment1/Q2/codes/myHM.m
@@ -1,6 +1,9 @@
-function myHM(img_name, img_ref_name)
-  img = imread(img_name);
-  img_ref = imread(img_ref_name);
+function myHM()
+  
+  img = imread('../data/retina.png');
+  img_ref = imread('../data/retinaRef.png');
+  mask = imread('../data/retinaMask.png');
+  refmask = imread('../data/retinaRefMask.png');
   
   [w,h,d] = size(img);
   [wr, hr, ~] = size(img_ref);
@@ -10,19 +13,31 @@ function myHM(img_name, img_ref_name)
   j=1:h;
   
   for k=1:d
-    x = reshape(img(i,j,k), 1, w*h);
-    hist_img = histc(x, 0:255);
+      
+    x = img_ref(:,:,k);
+    x(refmask == 0) = -1;
+    x = reshape(img(:,:,k), 1, w*h);
+    hist_img = histc(x, -1:255);
+    hist_img = hist_img(2:257);
     cdf = cumsum(hist_img);
     cdf = cdf/cdf(256);
-     
+    
+    x = img_ref(:,:,k);
+    x(mask == 0) = -1;
     x = reshape(img_ref(:,:,k), 1, wr*hr);
-    hist_ref = histc(x, 0:255);
+    hist_ref = histc(x, -1:255);
+    hist_ref = hist_ref(2:257);
     cdf_ref = cumsum(hist_ref);
     cdf_ref = cdf_ref/cdf_ref(256);
-    cdf_inv = icdf(cdf_ref, cdf(:));
+    
+    cdf_inv = zeros(1,256);
+    for l=1:256
+        [~,cdf_inv(1,l)]=min(abs(cdf_ref-cdf(1,l)));
+    end
     
     img_out(i,j,k) = cdf_inv(img(i,j,k)+1);
   end
   visualize(img_out);
+  visualize(img);
   
 end
