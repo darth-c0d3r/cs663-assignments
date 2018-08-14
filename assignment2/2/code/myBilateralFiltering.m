d6414b3b7de0424a6caa89fc7c763d445e7a3998
darth-c0d3r <gurparkashsohi@gmail.com>
2018-08-14 22:35:47 +0530

    q2 done (only for barbara)

--- assignment2/2/code/myBilateralFiltering.m
+++ assignment2/2/code/myBilateralFiltering.m
@@ -1,9 +1,9 @@
 function myBilateralFiltering(img_name)
     
 %     parameters
-    k_size = 5;
-    sigma_space = 1;
-    sigma_intensity = 1;
+    k_size = 11;
+    sigma_space = 2.0;
+    sigma_intensity = 10.0;
     
     image_struct = load(img_name);
     img = image_struct.imageOrig;
@@ -12,25 +12,42 @@ function myBilateralFiltering(img_name)
     img_noisy = img+noise;
     
     space_filter = fspecial('gaussian', k_size, sigma_space);
-%     imagesc(mat2gray(space_filter));
     img_out = img_noisy;
-    for i=k_size:(h-k_size)
-        for j=k_size:(w-k_size)
-            im_part = img_noisy(i:i+k_size-1,j:j+k_size-1);
+    sz = ceil((k_size-1)/2);
+    for i=1:h
+        for j=1:w
+            i_min = max(1, i-sz);
+            i_max = min(h, i+sz);
+            j_min = max(1, j-sz);
+            j_max = min(w, j+sz);
+            il = i_min - i + sz + 1;
+            jl = j_min - j + sz + 1;
+            ir = 2*sz + 1 - ((i+sz)-(i_max));
+            jr = 2*sz + 1 - ((j+sz)-(j_max));
+            im_part = img_noisy(i_min:i_max,j_min:j_max);
             int_filter = normpdf(im_part,img_noisy(i,j),sigma_intensity);
-            filter = int_filter .* space_filter;
+            filter = int_filter .* (space_filter(il:ir, jl:jr));
             numer = sum(sum(filter .* im_part));
             denom = sum(sum(filter));
-            img_out(i+floor(k_size/2),j+floor(k_size/2)) = numer/denom;
+            img_out(i,j) = numer/denom;
         end
     end
-    figure, imshow(mat2gray(img_out));
-    figure, imshow(mat2gray(img_noisy));
+%     figure, imshow(mat2gray(img_out));
+%     figure, imshow(mat2gray(img_noisy));
+    iptsetpref('ImshowAxesVisible','on');
+    figure('units','normalized','outerposition',[0 0 1 1])
+    subplot(1,3,1);
+    imshow(mat2gray(img)), colorbar;
+    title('Input Image')
+    subplot(1,3,2);
+    imshow(mat2gray(img_noisy)), colorbar;
+    title('Noisy Image')
+    subplot(1,3,3);
+    imshow(mat2gray(img_out)), colorbar;
+    title('Filtered Image')
     
+%     rmsd calculation
     rmsd = (img - img_out);
-    rmsd = sqrt((sum(sum(rmsd.*rmsd)))/(h*w));
+    rmsd = sqrt((sum(sum(rmsd.^2)))/(h*w));
     disp(rmsd);
-%     rmsd = (img - img_noisy);
-%     rmsd = sqrt((sum(sum(rmsd.*rmsd)))/(h*w));
-%     disp(rmsd);
 end
