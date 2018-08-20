13c019a7324936b23f70864fb0c80f7d09c3bada
v-zion <anmol.9822@gmail.com>
2018-08-20 21:01:22 +0530

    q1 done, parameter tuning left

--- assignment2/1/code/myMainScript.m
+++ assignment2/1/code/myMainScript.m
@@ -3,4 +3,13 @@
 tic;
 %% Your code here
 
+myUnsharpMasking('../data/lionCrop.mat', 0.25, 5);
+myUnsharpMasking('../data/lionCrop.mat', 0.5, 5);
+myUnsharpMasking('../data/lionCrop.mat', 0.75, 5);
+
+myUnsharpMasking('../data/lionCrop.mat', 0.25, 3);
+myUnsharpMasking('../data/lionCrop.mat', 0.5, 3);
+myUnsharpMasking('../data/lionCrop.mat', 0.75, 3);
+
 toc;
+
---
+++ assignment2/1/code/myUnsharpMasking.m
@@ -0,0 +1,21 @@
+function [imgOutput] = myUnsharpMasking(img_in, scale, radius)
+    
+    img = load(img_in);
+    imgIn = img.imageOrig;
+
+    imgInput = mat2gray(myLinearContrastStretching(mat2gray(imgIn)));
+    
+    gaussian = fspecial('gaussian', [15,15], radius);
+    img_blurred = imfilter(imgIn, gaussian, 'conv');
+    output_mat = imgIn + scale*(imgIn - img_blurred);
+    imgOutput = mat2gray(myLinearContrastStretching(mat2gray(output_mat)));
+    
+    figure;
+    subplot(1,2,1);
+    imshow(imgInput), colorbar;
+    title('Original Image');
+    subplot(1,2,2);
+    imshow(imgOutput), colorbar;
+    title('Sharpened Image');
+    
+end
