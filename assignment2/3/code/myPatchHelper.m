44de10851ad1969ccbf41d1427bee1cf3f4902e0
Rajat Rathi <30855764+rathi22@users.noreply.github.com>
2018-08-20 20:53:54 +0530

    Update myPatchHelper.m

--- assignment2/3/code/myPatchHelper.m
+++ assignment2/3/code/myPatchHelper.m
@@ -1,32 +1,12 @@
 function res = myPatchHelper(window, h)
-% <<<<<<< HEAD
-%     g_filter = fspecial('gaussian',9,2);
-%     center_patch = window(13:21,13:21);
-%     center_patch = center_patch.*g_filter;
-%     A = im2col(window, [9 9], 'sliding');
-%     B = repmat(center_patch(:), [1 625]);
-%     C = repmat(g_filter(:), [1 625]);
-%     weights = exp(-sum((A.*C-B).^2)/(h*h));
-%     center_intensities = A(41,:);
-%     res = sum(weights.*center_intensities)/sum(weights);
-% =======
-    center_patch = window(13:21,13:21);
-    g = @(X) exp(-sum(sum((X-center_patch).^2))/(h*h));  
-%     weights = nlfilter(window, [9 9], g);
-    for i=5:29
-        for j=5:29
-            weights(i,j) = g(window(i-4:i+4,j-4:j+4));
-        end
-    end
-%     Removing boundaries
-    weights(1:4,:) = 0;
-    weights(30:33,:) = 0;
-    weights(:,1:4) = 0;
-    weights(:,30:33) = 0;
-
-%     Removing center pixel
-    weights(17,17) = 0;
-    
-    res = sum(sum(weights.*window))/sum(sum(weights));
-% >>>>>>> e3a4e45e7a82db793f4e0219e302c659c75e7f04
+    g_filter = fspecial('gaussian',9,2);
+    A = im2col(window, [9 9], 'sliding');
+    B = repmat(A(:,ceil(end/2)), [1 size(A,2)]);
+    C = repmat(g_filter(:), [1 size(A,2)]);
+    D = (A-B).*C;
+%     Scaling so that parameter can have high value
+    D = D*100;
+    weights = exp(-sum(D.*D)/(h*h));
+    center_intensities = A(ceil(end/2),:);
+    res = sum(weights.*center_intensities)/sum(weights);
 end
