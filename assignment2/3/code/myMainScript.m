ad775ea758b3bf8bc93c891ff5d888c6ce5dafd1
Rajat Rathi <30855764+rathi22@users.noreply.github.com>
2018-08-20 20:54:32 +0530

    Updated main script

--- assignment2/3/code/myMainScript.m
+++ assignment2/3/code/myMainScript.m
@@ -3,11 +3,26 @@
 tic;
 %% Your code here
 
-var_par = 0.009:0.001:0.010;
+% Converting intensity range of both images to [0,1].
+
+% img_real = im2double(imread('../data/grass.png'));
+% imgStruct = load('../data/grassNoisy.mat');
+% img_noisy = im2double(imgStruct.imgCorrupt);
+
+imgStruct = load('../data/barbara.mat');
+img = imgStruct.imageOrig;
+[h,w] = size(img);
+noise = randn(h,w)*(max(max(img))-min(min(img)))*0.05;
+img_real = im2double(img);
+img_noisy = im2double(img+noise);
+
+var_par = 1:1:10;
 for par = var_par
-    myPatchBasedFiltering('../data/grassNoisy.mat',par);
+    myPatchBasedFiltering(img_real,img_noisy,par);
+    toc;
 end    
     
-% 0.010 for grassNoisy
-% 0.007 for honeyComb
+% 0.84 for grassNoisy
+% 0.9 for honeyComb
+
 toc;
