f6da7caeb294bd3fe9140dd50e487c20861773af
v-zion <anmol.9822@gmail.com>
2018-08-25 12:43:39 +0530

    assignment3 upload

--- assignment2/1/code/myMainScript.m
+++ assignment2/1/code/myMainScript.m
@@ -1,13 +1,8 @@
+
 %% MyMainScript
 
 tic;
-%% Your code here
-
-myUnsharpMasking('../data/lionCrop.mat', 1, 2);
 myUnsharpMasking('../data/lionCrop.mat', 2, 2);
-myUnsharpMasking('../data/lionCrop.mat', 0.7, 2);
-myUnsharpMasking('../data/lionCrop.mat', 0.5, 2);
-myUnsharpMasking('../data/lionCrop.mat', 0.3, 2);
+myUnsharpMasking('../data/superMoonCrop.mat', 0.1, 1);
 
 toc;
-
