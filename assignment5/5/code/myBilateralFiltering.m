27fe7d40e391cd740aeb6ca197d474a6635ef957
v-zion <anmol.9822@gmail.com>
2018-11-01 23:54:45 +0530

    bilateral filtering done

--- assignment5/5/code/myBilateralFiltering.m
+++ assignment5/5/code/myBilateralFiltering.m
@@ -1,17 +1,37 @@
-function [img_out, rmse] = myBilateralFiltering(im, im1, sigma_space, sigma_intensity)
-	
-	[rows, cols] = size(im);
-	img_out = zeros(rows, cols);
+function [img_out, rmse] = myBilateralFiltering(img, img_noisy, sigma_space, sigma_intensity)
+    
+    k_size = 11;
+    [h, w] = size(img);
+    
+    space_filter = fspecial('gaussian', k_size, sigma_space);
+    img_out = img_noisy;
+    sz = ceil((k_size-1)/2);
+    for i=1:h
+        for j=1:w
+            i_min = max(1, i-sz);
+            i_max = min(h, i+sz);
+            j_min = max(1, j-sz);
+            j_max = min(w, j+sz);
+            il = i_min - i + sz + 1;
+            jl = j_min - j + sz + 1;
+            ir = 2*sz + 1 - ((i+sz)-(i_max));
+            jr = 2*sz + 1 - ((j+sz)-(j_max));
+            im_part = img_noisy(i_min:i_max,j_min:j_max);
+            int_filter = normpdf(im_part,img_noisy(i,j),sigma_intensity);
+            filter = int_filter .* (space_filter(il:ir, jl:jr));
+            filter = filter ./ sum(sum(filter));
+            img_out(i,j) = sum(sum(filter .* im_part));
+        end
+    end
+    rmse = (img - img_out);
+    rmse = sqrt((sum(sum(rmse.^2)))/(h*w));
 
-	for i = 1:rows
-		for j = 1:cols
-			
-
-	figure;
-	subplot(1,2,1);
-	imshow(mat2gray(im1));
-	title('Noised Image');
-	subplot(1,2,2);
-	imshow(mat2gray(img_out));
+    figure;
+    subplot(1,2,1);
+    imshow(mat2gray(img_noisy));
+    title('Noisy Image');
+    subplot(1,2,2);
+    imshow(mat2gray(img_out));
 	title(['Bilateral Filtered Image | rmse = ', num2str(rmse)]);
+
 end
--- assignment5/5/code/myMainScript.m
+++ assignment5/5/code/myMainScript.m
@@ -9,20 +9,20 @@ patch_size = 7;
 sigma = 20;
 sigma_intensity = 10;
 
-rmse_PCADenoising1 = myPCADenoising1(im, im1, patch_size, sigma)
-rmse_PCADenoising2 = myPCADenoising2(im, im1, patch_size, sigma, 0)
+% rmse_PCADenoising1 = myPCADenoising1(im, im1, patch_size, sigma)
+% rmse_PCADenoising2 = myPCADenoising2(im, im1, patch_size, sigma, 0)
 
-% [im2, rmse_BilateralFiltered] = myBilateralFiltering(im, im1, sigma, sigma_intensity)
+[im2, rmse_BilateralFiltered] = myBilateralFiltering(im, im1, sigma, sigma_intensity)
 
 %% Poisson Distributed Noise
 % The first case is much better than the second case where im has been divided by 20.
 % This is because in the second case the noise introduced is much greater
 % than the first case and hence, a comparitive bad restoration.
 
-im_poisson1 = sqrt(im) + randn(size(im))*0.25;
-rmse_Poisson1 = myPCADenoising2(im, im_poisson1, patch_size, 1/4, 1)
+% im_poisson1 = sqrt(im) + randn(size(im))*0.25;
+% rmse_Poisson1 = myPCADenoising2(im, im_poisson1, patch_size, 1/4, 1)
 
-im_poisson2 = sqrt(im)./sqrt(20) + randn(size(im))*0.25;
-rmse_Poisson2 = myPCADenoising2(im, im_poisson2, patch_size, 1/4, 2)
+% im_poisson2 = sqrt(im)./sqrt(20) + randn(size(im))*0.25;
+% rmse_Poisson2 = myPCADenoising2(im, im_poisson2, patch_size, 1/4, 2)
 
 toc;
